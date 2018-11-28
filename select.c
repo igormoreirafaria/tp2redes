7e8470b01eb9f06ccef817112e8b78a342d43ea7
AnnaAguilar <34386951+AnnaAguilar@users.noreply.github.com>
2018-11-28 14:55:11 -0200

    identação

--- select.c
+++ select.c
@@ -10,9 +10,8 @@
 #include <arpa/inet.h>
 /* port we're listening on */
 #define PORT 8080
- 
-int main(int argc, char *argv[])
-{
+
+int main(int argc, char *argv[]){
     /* master file descriptor list */
     fd_set master;
     /* temp file descriptor list for select() */
@@ -39,16 +38,14 @@ int main(int argc, char *argv[])
     FD_ZERO(&read_fds);
 
     /* get the listener */
-    if((listener = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {
+    if((listener = socket(AF_INET, SOCK_STREAM, 0)) == -1){
         perror("Server-socket() error lol!");
         /*just exit lol!*/
         exit(1);
     }
     printf("Server-socket() is OK...\n");
     /*"address already in use" error message */
-    if(setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-    {
+    if(setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){
         perror("Server-setsockopt() error lol!");
         exit(1);
     }
@@ -60,16 +57,14 @@ int main(int argc, char *argv[])
     serveraddr.sin_port = htons(PORT);
     memset(&(serveraddr.sin_zero), '\0', 8);
 
-    if(bind(listener, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1)
-    {
+    if(bind(listener, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1){
         perror("Server-bind() error lol!");
         exit(1);
     }
     printf("Server-bind() is OK...\n");
 
     /* listen */
-    if(listen(listener, 10) == -1)
-    {
+    if(listen(listener, 10) == -1){
         perror("Server-listen() error lol!");
         exit(1);
     }
@@ -81,72 +76,52 @@ int main(int argc, char *argv[])
     fdmax = listener; /* so far, it's this one*/
 
     /* loop */
-    for(;;)
-    {
+    for(;;){
         /* copy it */
         read_fds = master;
 
-        if(select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1)
-        {
+        if(select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1){
             perror("Server-select() error lol!");
             exit(1);
         }
         printf("Server-select() is OK...\n");
 
         /*run through the existing connections looking for data to be read*/
-        for(i = 0; i <= fdmax; i++)
-        {
-            if(FD_ISSET(i, &read_fds))
-            { /* we got one... */
-                if(i == listener)
-                {
+        for(i = 0; i <= fdmax; i++){
+            if(FD_ISSET(i, &read_fds)){ /* we got one... */
+                if(i == listener){
                     /* handle new connections */
                     addrlen = sizeof(clientaddr);
-                    if((newfd = accept(listener, (struct sockaddr *)&clientaddr, &addrlen)) == -1)
-                    {
+                    if((newfd = accept(listener, (struct sockaddr *)&clientaddr, &addrlen)) == -1){
                         perror("Server-accept() error lol!");
-                    }
-                    else
-                    {
+                    }else{
                         printf("Server-accept() is OK...\n");
 
                         FD_SET(newfd, &master); /* add to master set */
-                        if(newfd > fdmax)
-                        { /* keep track of the maximum */
+                        if(newfd > fdmax){ /* keep track of the maximum */
                             fdmax = newfd;
                         }
                         printf("%s: New connection from %s on socket %d\n", argv[0], inet_ntoa(clientaddr.sin_addr), newfd);
                     }
-                }
-                else
-                {
+                }else{
                     /* handle data from a client */
-                    if((nbytes = recv(i, buf, sizeof(buf), 0)) <= 0)
-                    {
+                    if((nbytes = recv(i, buf, sizeof(buf), 0)) <= 0){
                         /* got error or connection closed by client */
-                        if(nbytes == 0)
-                            /* connection closed */
-                            printf("%s: socket %d hung up\n", argv[0], i);
-
-                        else
-                            perror("recv() error lol!");
+                        /* connection closed */
+                        if(nbytes == 0) printf("%s: socket %d hung up\n", argv[0], i); /* connection closed */
+                        else perror("recv() error lol!");
 
                         /* close it... */
                         close(i);
                         /* remove from master set */
                         FD_CLR(i, &master);
-                    }
-                    else
-                    {
+                    }else{
                         /* we got some data from a client*/
-                        for(j = 0; j <= fdmax; j++)
-                        {
+                        for(j = 0; j <= fdmax; j++){
                             /* send to everyone! */
-                            if(FD_ISSET(j, &master))
-                            {
+                            if(FD_ISSET(j, &master)){
                                 /* except the listener and ourselves */
-                                if(j != listener && j != i)
-                                {
+                                if(j != listener && j != i){
                                     if(send(j, buf, nbytes, 0) == -1)
                                         perror("send() error lol!");
                                 }
